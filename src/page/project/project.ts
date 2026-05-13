12a1b84048b22ed6e494be16d9e40c695436f3ac
Chey Kimirth <cheykimrith123@gmail>
2026-05-13 13:18:31 +0700

    done V1

--- src/page/project/project.ts
+++ src/page/project/project.ts
@@ -13,7 +13,7 @@ export class Project {
       title: 'Hotel Rental System',
       description:
         'A robust hospitality platform featuring RBAC dashboards for Admins, Owners, and Customers. Integrated with Google OAuth 2.0 for seamless SSO and secure payment gateways for real-time bookings.',
-      image: 'assets/projects/hotel.jpg',
+      image: '/profile/hotelrental.png',
       tech: ['Java Thymeleaf', 'PostgreSQL', 'Spring Security'],
       link: '#',
       github: '#',
@@ -22,7 +22,7 @@ export class Project {
       title: 'POS & QR Order System',
       description:
         'Cafe management system with KHQR integration for real-time mobile payments and QR-based ordering.',
-      image: 'assets/projects/pos.jpg',
+      image: '/profile/ordersystem.png',
       tech: ['.NET Core', 'ReactJs', 'KHQR API', 'SQL Server'],
       link: '#',
       github: '#',
@@ -31,7 +31,7 @@ export class Project {
       title: 'Real-time Game Chat',
       description:
         'Multiplayer game platform featuring instant messaging and global chat rooms using WebSocket technology.',
-      image: 'assets/projects/chat.jpg',
+      image: '/profile/chat&game.png',
       tech: ['React JS', 'Node.js', 'Socket.io', 'MongoDB'],
       link: '#',
       github: '#',
